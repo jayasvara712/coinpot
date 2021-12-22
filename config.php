14fc6ec7c755cd867a8726e607cc47db8a8c6718
jayasvara712 <36564378+jayasvara712@users.noreply.github.com>
2021-12-22 23:40:52 +0800

    2
    2

--- config.php
+++ config.php
@@ -3,4 +3,4 @@
 $user = "Mozilla/5.0 (Linux; Android 7.0; Redmi Note 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.46 Mobile Safari/537.36";
 
 // Cookie Dasbord
-$cookie = "ci_session=lcdd4r35nhfqoqsd6cn7vm262tcbmaat;1r728-0=seen;bitmedia_fid=eyJmaWQiOiI5YzU2ZjE0MjAyNzQ5NTE1MTEwMDJkOTE2MGI0MDI0NyIsImZpZG5vdWEiOiI2NzBjZDc2MWM2OTFmNTU5ODZhZGVhOTAyZmY0ZGExNyJ9;csrf_cookie_name=c09bdf3cb85527cf755d427eef7b10e4;1r728-1=seen;2r728-0=seen;meta728=0;meta300=0;_ga_GK6K4JR3YW=GS1.1.1640186589.1.0.1640186589.0;_ga=GA1.1.902378601.1640186589;_data_cpc=15-1";
+$cookie = "ci_session=lcdd4r35nhfqoqsd6cn7vm262tcbmaat;bitmedia_fid=eyJmaWQiOiI5YzU2ZjE0MjAyNzQ5NTE1MTEwMDJkOTE2MGI0MDI0NyIsImZpZG5vdWEiOiI2NzBjZDc2MWM2OTFmNTU5ODZhZGVhOTAyZmY0ZGExNyJ9;csrf_cookie_name=c09bdf3cb85527cf755d427eef7b10e4;meta728=0;meta300=0;_ga=GA1.1.902378601.1640186589;1r728-0=seen;2r728-0=seen;_ga_GK6K4JR3YW=GS1.1.1640186589.1.1.1640187414.0;_data_cpc=15-1_190-1";
