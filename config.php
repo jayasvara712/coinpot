78d4c185a5e24cfbd6d8a35ce24c89ba28b8046d
jayasvara712 <36564378+jayasvara712@users.noreply.github.com>
2021-12-22 23:28:04 +0800

    config.php
    1234

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Isi Useragent Device kalian
+$user = "Mozilla/5.0 (Linux; Android 9; Redmi Note 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36";
+
+// Cookie Dasbord
+$cookie = "meta728=0; meta300=0; _ga=GA1.1.439616641.1639527841; bitmedia_fid=eyJmaWQiOiIxMjg3NjRlYTc1MGFiODcxMzc4NDQyMzYzMjk5OTZmNSIsImZpZG5vdWEiOiIxYmM2ZGMzNjlkM2RmNGNmMzY5NWJiMzdlMzVhNTlhOSJ9; 1r300-0=seen; 1r300-1=seen; csrf_cookie_name=7719e11377cd270f78f5d11fde68bf63; _data_cpc=286-1; 2r728-0=seen; ci_session=k42gb8eg1f8q838ub94am1auq7jr3dtb; 1r728-0=seen; 2r728-1=seen; _ga_GK6K4JR3YW=GS1.1.1639904685.11.1.1639905157.0";
